3be2e6134e5622cc51d5705cfca4498e6b8fff14
carolxavier <xavier.biotec@gmail.com>
2022-05-13 15:31:25 -0300

    Layout of SignInPage started

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom';
+import App from './Components/App';
+
+Render();
+export default function Render(){
+    ReactDOM.render(
+          <App />,
+        document.querySelector('.root')
+      );
+};
+
